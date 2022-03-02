554a8e64bcff5e64554335edc37f75ab834ea94b
Masanori Ohgita <mp@ohgita.info>
2022-03-03 05:28:08 +0900

    Fix: blank article in article list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ScullyRoutesService } from '@scullyio/ng-lib';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,11 @@ import { ScullyRoutesService } from '@scullyio/ng-lib';
   styleUrls: ['./home.component.scss', '../article/article-card.scss'],
 })
 export class HomeComponent implements OnInit {
-  links$ = this.scully.available$;
+  links$ = this.scully.available$.pipe(
+    map((routes) => {
+      return routes.filter((route) => route.route !== '/');
+    }),
+  );
 
   constructor(private scully: ScullyRoutesService) {}
 
