6289c9128dd5cc21bc775c7df9e50e876ac573c4
Masanori Ohgita <mp@ohgita.info>
2022-03-10 03:31:25 +0900

    Change: Test runner - Karma to Jest
    Squashed commit of the following:
    
    commit a1de2b0244aa3336e559b1e3af507b55a8a2e9f7
    Author: Masanori Ohgita <mp@ohgita.info>
    Date:   Thu Mar 10 03:26:44 2022 +0900
    
        Fix: GitHub Actions - Wrong branch
    
    commit 42834c7fdeeeda059bbf78caa874707eec9ee3a6
    Author: Masanori Ohgita <mp@ohgita.info>
    Date:   Thu Mar 10 03:16:22 2022 +0900
    
        Add: GitHub Action - jest-coverage-report-action
    
    commit 00acbc71b01ebdf5679d2733713df09a7a85ac46
    Author: Masanori Ohgita <mp@ohgita.info>
    Date:   Thu Mar 10 03:13:43 2022 +0900
    
        Change: Test runnner - Karma to Jest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, AppModule],
       declarations: [AppComponent],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
     }).compileComponents();
   });
 
@@ -26,8 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'blog app is running!'
-    );
+    expect(compiled.querySelector('mat-toolbar.header')?.textContent).toContain('Masanori');
   });
 });
